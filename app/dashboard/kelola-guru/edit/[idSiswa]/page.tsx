bbf1aaa17b02e28c196c1b421c775ba35d46e647
AlulCode45 <alulgans70@gmail.com>
2024-06-18 20:05:17 +0700

    [ADD][UPDATE] Finish Dashboard Template Layout: 	new file:   app/dashboard/kelola-guru/add/page.tsx 	new file:   app/dashboard/kelola-guru/edit/[idSiswa]/page.tsx 	new file:   app/dashboard/kelola-guru/page.tsx 	new file:   app/dashboard/kelola-kelas/add/page.tsx 	new file:   app/dashboard/kelola-kelas/edit/[idSiswa]/page.tsx 	new file:   app/dashboard/kelola-kelas/page.tsx 	new file:   app/dashboard/kelola-siswa/add/page.tsx 	new file:   app/dashboard/kelola-siswa/edit/[idSiswa]/page.tsx 	modified:   app/dashboard/kelola-siswa/page.tsx 	modified:   app/dashboard/layout.tsx

---
+++ app/dashboard/kelola-guru/edit/[idSiswa]/page.tsx
@@ -0,0 +1,56 @@
+'use client'
+interface Student {
+    nisn: string;
+    nama: string;
+    nis: number;
+    tanggalLahir: string;
+    alamat: string;
+    jenisKelamin: string;
+}
+
+export default function EditSiswa() {
+    return (
+        <>
+            <div className="bg-white p-5 shadow rounded-md">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-xl">Tambah Siswa</h1>
+                    <button type="submit" className='bg-blue-400 text-white font-semibold px-3 py-2'>Simpan</button>
+                </div>
+                <form onSubmit={() => { }}>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                        <div className="my-2">
+                            <label htmlFor="" className="block text-sm mb-1">NISN</label>
+                            <input type="number" className="w-full p-2 border focus:outline-blue-200 focus:shadow-blue-400" placeholder="Masukan NISN" />
+                        </div>
+
+                        <div className="my-2">
+                            <label htmlFor="" className="block text-sm mb-1">NIS</label>
+                            <input type="text" className="w-full p-2 border focus:outline-blue-200 focus:shadow-blue-400" placeholder="Masukan NIS" />
+                        </div>
+                    </div>
+                    <div className="my-2">
+                        <label htmlFor="" className="block text-sm mb-1">Nama Lengkap</label>
+                        <input type="number" className="w-full p-2 border focus:outline-blue-200 focus:shadow-blue-400" placeholder="Masukan Nama Lengkap" />
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                        <div className="my-2">
+                            <label htmlFor="" className="block text-sm mb-1">Tanggal Lahir</label>
+                            <input type="date" className="w-full p-2 border focus:outline-blue-200 focus:shadow-blue-400" placeholder="Masukan Tanggal Lahir" />
+                        </div>
+                        <div className="my-2">
+                            <label htmlFor="" className="block text-sm mb-1">Jenis Kelamin</label>
+                            <select className="w-full p-2 border focus:outline-blue-200 focus:shadow-blue-400">
+                                <option value="Laki-Laki">Laki-Laki</option>
+                                <option value="Perempuan">Perempuan</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="my-2">
+                        <label htmlFor="" className="block text-sm mb-1">Alamat</label>
+                        <textarea className="w-full p-2 border focus:outline-blue-200 focus:shadow-blue-400" placeholder="Masukan Alamat" />
+                    </div>
+                </form>
+            </div>
+        </>
+    )
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,10 +6,8 @@ import Topbar from "@/components/Topbar";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
 
-
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import { FilterMatchMode, PrimeReactProvider } from "primereact/api";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,24 +20,16 @@ export default function RootLayout({ children }: Readonly<{
     // Menentukan menu yang aktif berdasarkan URL
     const getActiveMenu = () => {
         if (path === "/dashboard") return 0;
-        if (path === "/dashboard/kelola-siswa") return 1;
-        if (path === "/dashboard/kelola-kelas") return 2;
-        if (path === "/dashboard/kelola-guru") return 3;
+        if (path.startsWith("/dashboard/kelola-siswa")) return 1; // Deteksi semua turunan dari /dashboard/kelola-siswa
+        if (path.startsWith("/dashboard/kelola-kelas")) return 2; // Deteksi semua turunan dari /dashboard/kelola-kelas
+        if (path.startsWith("/dashboard/kelola-guru")) return 3; // Deteksi semua turunan dari /dashboard/kelola-guru
         return -1;
     };
 
     const menuActive = getActiveMenu();
 
     return (
-        <PrimeReactProvider
-        // value={{
-        // filterMatchModeOptions: {
-        //     text: [FilterMatchMode.STARTS_WITH, FilterMatchMode.CONTAINS, FilterMatchMode.NOT_CONTAINS, FilterMatchMode.ENDS_WITH, FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS],
-        //     numeric: [FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.LESS_THAN, FilterMatchMode.LESS_THAN_OR_EQUAL_TO, FilterMatchMode.GREATER_THAN, FilterMatchMode.GREATER_THAN_OR_EQUAL_TO],
-        //     date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_BEFORE, FilterMatchMode.DATE_AFTER]
-        // },
-        // }}
-        >
+        <PrimeReactProvider>
             <html lang="en">
                 <body className={inter.className}>
                     <div className="lg:flex h-screen">
